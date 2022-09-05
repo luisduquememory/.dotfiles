4660ffcff32f1a34798ee9e663edc466c1e438fd
Luis Duque <luisfpetrucci@gmail.com>
2022-09-05 17:27:37 -0500

    docs(fish): added comment to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -11,10 +11,12 @@ end
 # vim keybindings
 fish_vi_key_bindings
 function fish_user_key_bindings
-    for mode in insert default visual
-        bind -M $mode \cf forward-word
-    end
+   for mode in insert default visual
+       bind -M $mode \cf forward-word
+   end
 end
+# uncomment if you want the default keybindings
+#fish_default_key_bindings
 
 # fish welcome message
 set fish_greeting
