be3228b8e41de8d49a24171d78c8a648a107db4c
Luis Duque <luisfpetrucci@gmail.com>
2022-09-19 20:53:27 -0500

    feat(fish): deleted unused function and added one to debug tasks

--- fish/conf.d/swim.fish
+++ fish/conf.d/swim.fish
@@ -2,36 +2,21 @@ abbr genin "swsdk task generate-inputs -p"
 abbr genout "swsdk task generate-outputs"
 abbr stest "swsdk integration test -s"
 abbr swb "swsdk integration build -s -v 3.7"
-abbr swbump "swsdk integration update"
 abbr swg "swsdk task run -s  --generate"
 abbr swr "swsdk task run -s --verbose"
 abbr swv "swsdk integration validate -s"
 
-function sdkdev -d "link to local swsdk"
-    $HOME/code/swimlane/swimlane-sdk-cli/bin/run $argv
-end
-
-function swrl -d "run a task with the local run container"
-    # the argument is the integration folder name
-	# 1. Build the container
+function swd -d "debug task with local container"
+    # select the task to debug
+    set task (path basename (ls ./imports/**.py  | fzf)) # see https://fishshell.com/docs/current/cmds/path.html
+    set task (path change-extension '' $task)
+	# Build the container
     echo "Building docker image"
 	docker build -f $HOME/code/swimlane/sdk-run/Dockerfile --build-arg PYTHON_VERSION=3.7  -t local/sdk-cli-run:3.7 $HOME/code/swimlane/sdk-run
     echo "Saving imagen ID"
 	set RUNIMAGE (docker images -a | grep local/sdk-cli-run | awk '{print $3}')
-	echo $RUNIMAGE
-    set integration "/Users/luis.duque/code/swimlane-plugins/$argv"
-    echo "Running integration in $integration"
-	swsdk task:run -d $integration -s -i $RUNIMAGE --verbose
-end
-
-function swri -d "build a local docker run image and return the image ID"
-    # the argument is the integration folder name
-	# 1. Build the container
-    echo "Building docker image"
-	docker build -f $HOME/code/swimlane/sdk-run/Dockerfile --build-arg PYTHON_VERSION=3.7  -t local/sdk-cli-run:3.7 $HOME/code/swimlane/sdk-run
-    echo "Saving imagen ID"
-	set RUNIMAGE (docker images -a | grep local/sdk-cli-run | awk '{print $3}')
-	echo $RUNIMAGE
+	echo "Running task with local image and debug flag"
+    swsdk task run -s --verbose -n $task -s -i $RUNIMAGE --debug code
 end
 
 function scd -d "fuzzy find a plugin and cd into it"
@@ -47,10 +32,6 @@ end
 
 function tasks -d "return a list of tasks names and corresponding manifests on a integration"
     node $HOME/Documents/scripts/tasks_names.js
-end
-
-function flat -d "flat a payload and put in the clipboard"
-    python $HOME/Documents/scripts/flat_payload.py
 end
 
 function gensteps -d "generate test steps"
