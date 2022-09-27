7627c475bb310d70faef83b5a21956b4ab09b9de
Luis Duque <luisfpetrucci@gmail.com>
2022-09-27 17:06:22 -0500

    fix(emacs): bd keybinding

--- emacs.d/keybindings.el
+++ emacs.d/keybindings.el
@@ -13,7 +13,7 @@
   :keymaps 'override ;; see https://github.com/noctuid/general.el/issues/99#issuecomment-360914335
   :prefix "SPC"
   "." 'find-file
-  "bd" 'kill-this-buffer
+  "bd" 'kill-buffer-and-window
   "d" 'dap-hydra
   "p" 'projectile-command-map
   "SPC" 'projectile-find-file
